f88dfa5e28e61bb49e8ed8c86c40985024355d50
Nolan Romero <nolanrom@icloud.com>
2023-09-25 20:12:49 +0000

    Im done

--- TicTakToe.py
+++ TicTakToe.py
@@ -8,11 +8,12 @@ if pl1 == "X":
     pl2 = "O"
 else:
      pl2 = "X"
-def PlayGame(numrounds):
+
+def PlayGame(pl1, pl2):
     while True:
 
         response = input(" where go ")
-        #numbers = [1,asdfghjk,]
+        
         if board[int(response)] != 0:
             print(" Don't do that!! ")
         else: 
@@ -20,7 +21,30 @@ def PlayGame(numrounds):
         print(board[0],board[1],board[2])
         print(board[3],board[4],board[5])
         print(board[6],board[7],board[8])
-
+        if board[0] == board[1] == board[2] == "X":
+            print(" X wins!! ")
+            return board
+        elif board[3] == board[4] == board[5] == "X":
+            print(" X wins!! ")
+            return board
+        elif board[6] == board[7] == board[8] == "X":
+            print(" X wins!! ")
+            return board
+        elif board[0] == board[4] == board[8] == "X":
+            print(" X wins!! ")
+            return board
+        elif board[2] == board[4] == board[6] == "X":
+            print(" X wins!! ")
+            return board
+        elif board[0] == board[3] == board[6] == "X":
+            print(" X wins!! ")
+            return board
+        elif board[1] == board[4] == board[7] == "X":
+            print(" X wins!! ")
+            return board
+        elif board[2] == board[5] == board[8] == "X":
+            print(" X wins!! ")
+            return board
         response = input(" where go ")
 
         if board[int(response)] != 0:
@@ -31,39 +55,33 @@ def PlayGame(numrounds):
         print(board[3],board[4],board[5])
         print(board[6],board[7],board[8])    
 
-        if board[0] and board[1] and board[2] == "X":
-            print(" X wins!! ")
-        elif board[3] and board[4] and board[5] == "X":
-            print(" X wins!! ")
-        elif board[6] and board[7] and board[8] == "X":
-            print(" X wins!! ")
-        elif board[0] and board[4] and board[8] == "X":
-            print(" X wins!! ")
-        elif board[2] and board[4] and board[6] == "X":
-            print(" X wins!! ")
-        elif board[0] and board[3] and board[6] == "X":
-            print(" X wins!! ")
-        elif board[1] and board[4] and board[7] == "X":
-            print(" X wins!! ")
-        elif board[2] and board[5] and board[8] == "X":
-            print(" X wins!! ")
-        elif board[0] and board[1] and board[2] == "O":
-            print(" O wins!! ")
-        elif board[3] and board[4] and board[5] == "O":
-            print(" O wins!! ")
-        elif board[6] and board[7] and board[8] == "O":
-            print(" O wins!! ")
-        elif board[0] and board[4] and board[8] == "O":
-            print(" O wins!! ")
-        elif board[2] and board[4] and board[6] == "O":
-            print(" O wins!! ")
-        elif board[0] and board[3] and board[6] == "O":
-            print(" O wins!! ")
-        elif board[1] and board[4] and board[7] == "O":
-            print(" O wins!! ")
-        elif board[2] and board[5] and board[8] == "O":
-            print(" O wins!! ")            
 
-PlayGame(3)
-PlayGame(5)
+        if board[0] == board[1] == board[2] == "O":
+            print(" O wins!! ")
+            return board
+        elif board[3] == board[4] == board[5] == "O":
+            print(" O wins!! ")
+            return board
+        elif board[6] == board[7] == board[8] == "O":
+            print(" O wins!! ")
+            return board
+        elif board[0] == board[4] == board[8] == "O":
+            print(" O wins!! ")
+            return board
+        elif board[2] == board[4] == board[6] == "O":
+            print(" O wins!! ")
+            return board
+        elif board[0] == board[3] == board[6] == "O":
+            print(" O wins!! ")
+            return board
+        elif board[1] == board[4] == board[7] == "O":
+            print(" O wins!! ")
+            return board
+        elif board[2] == board[5] == board[8] == "O":
+            print(" O wins!! ")    
+            return board        
 
+
+
+
+PlayGame(pl1,pl2)
